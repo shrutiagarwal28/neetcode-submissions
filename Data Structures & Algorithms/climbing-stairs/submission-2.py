c0d7faa5c496dea1af32936d87e9d4f3936e531b
Shruti Agarwal <35076210+shrutiagarwal28@users.noreply.github.com>
2026-09-15 01:45:15 -0400

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+    #    Top Down:
+        # cache = [-1]*n
+
+        # def dfs(node):
+        #     if node >= n:
+        #         return node == n
+
+        #     if cache[node] != -1:
+        #         return cache[node]
+
+        #     cache[node] = dfs(node+1) + dfs(node+2)
+
+        #     return cache[node]
+        
+        # return dfs(0)
+
+    #    Bottom Up:
+        if n <= 2:
+            return n
+        dp = [0]*(n+1)
+        dp[1], dp[2] = 1, 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+        
